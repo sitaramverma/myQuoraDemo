42253e5d871570156a88f742221d68a7d16177cf
Sitaram <sitaram@xelpmoc.in>
2019-01-08 19:42:21 +0530

    first commit

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,5 @@
 export const configurationFile = {
   development: {
-    mongoUrl: "mongodb://localhost:27017/airtelTest?authMechanism=DEFAULT"
+    mongoUrl: "mongodb://localhost:27017/db_myQuoraDemo?authMechanism=DEFAULT"
   }
 };
